2c579380f3336130893e27497446daa1d77ecc6e
James Messinger <james.messinger@gmail.com>
2018-11-07 08:43:59 -0600

    Added a "busy" flag, which causes a spinner to appear

--- _ts/components/add-account/add-account.tsx
+++ _ts/components/add-account/add-account.tsx
@@ -3,18 +3,21 @@ import { AddAccountProps } from "./props";
 
 interface State {
   accountName: string;
+  busy: boolean;
 }
 
 export class AddAccount extends React.Component<AddAccountProps, State> {
   public readonly state: State = {
     accountName: "",
+    busy: false,
   };
 
   public render() {
     let { submitButtonText } = this.props;
+    let { accountName, busy } = this.state;
 
     return (
-      <form className="add-account form" onSubmit={this.handleSubmit}>
+      <form className={`add-account form ${busy ? "busy" : ""}`} onSubmit={this.handleSubmit}>
         <dl className="form-group">
           <dt className="input-label">
             <label htmlFor="repo_owner">GitHub Username</label>
@@ -22,12 +25,14 @@ export class AddAccount extends React.Component<AddAccountProps, State> {
           <dd className="input-field">
             <input type="text" name="account_name" className="form-control short"
               maxLength={100} autoFocus autoCapitalize="off" autoComplete="on" spellCheck={false}
-              placeholder="GitHub Username"
-              value={this.state.accountName} onChange={this.handleChange} />
+              placeholder="GitHub Username" disabled={busy}
+              value={accountName} onChange={this.handleChange} />
           </dd>
         </dl>
 
-        <button type="submit" className="btn btn-primary">{submitButtonText}</button>
+        <button type="submit" className="btn btn-primary" disabled={busy}>
+          {submitButtonText}
+        </button>
       </form>
     );
   }
@@ -36,12 +41,13 @@ export class AddAccount extends React.Component<AddAccountProps, State> {
     this.setState({ accountName: event.target.value });
   }
 
-  private handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  private handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (this.state.accountName) {
-      this.props.addAccount(this.state.accountName.trim());
-      this.setState({ accountName: "" });
+      this.setState({ busy: true });
+      await this.props.addAccount(this.state.accountName.trim());
+      this.setState({ accountName: "", busy: false });
     }
   }
 }
