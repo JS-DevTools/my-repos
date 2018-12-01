fdc7e831f5ac23b54869a661815c6a451c36931b
James Messinger <james.messinger@gmail.com>
2018-12-01 04:42:01 -0600

    Added a hacky workaround for a weird scrolling bug

--- _ts/index.tsx
+++ _ts/index.tsx
@@ -3,3 +3,7 @@ import * as ReactDOM from "react-dom";
 import { App } from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("react-app"));
+
+// HACK: I'm not sure why, but the page scrolls to the bottom after the first render.
+// This workaround prevents that.
+window.scrollTo(0, 0);
